c721b3be83457ece5bcc0ed5f4a8e1a01efafc51
Artem <artemtyschukk@gmail.com>
2023-09-19 16:02:53 +0200

    bug fixed, libraries update

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,148 +1,141 @@
-import React, {useState, useEffect, useRef, useMemo} from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect, useRef, useMemo } from "react";
+import PropTypes from "prop-types";
 
-import {motion} from "framer-motion/dist/framer-motion";
+import { motion } from "framer-motion";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import useMarvelService from "../../services/MarvelService";
 
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
-import useMarvelService from '../../services/MarvelService';
+import "./charList.scss";
 
-import './charList.scss';
-
-const setContent = (process, Component, newItemLoading) => { //FSM
-    switch (process) {
-        case 'waiting':
-            return <Spinner/>
-        case 'loading':
-            return newItemLoading ? <Component/> : <Spinner/>
-        case 'confirmed':
-            return <Component/>;
-        case 'error':
-            return <ErrorMessage/>;
-        default:
-            throw new Error('Unexpected process state')
-    }
-} 
+const setContent = (process, Component, newItemLoading) => {
+  //FSM
+  switch (process) {
+    case "waiting":
+      return <Spinner />;
+    case "loading":
+      return newItemLoading ? <Component /> : <Spinner />;
+    case "confirmed":
+      return <Component />;
+    case "error":
+      return <ErrorMessage />;
+    default:
+      throw new Error("Unexpected process state");
+  }
+};
 
 const CharList = (props) => {
+  const [charList, setCharList] = useState([]);
+  const [newItemLoading, setNewItemLoading] = useState(false);
+  const [offset, setOffset] = useState(210);
+  const [charEnded, setCharEnded] = useState(false);
 
-    const [charList, setCharList] = useState([]);
-    const [newItemLoading, setNewItemLoading] = useState(false);
-    const [offset, setOffset] = useState(210);
-    const [charEnded, setCharEnded] = useState(false);
-    
-    const {getAllCharacters, process, setProcess} = useMarvelService();
+  const { getAllCharacters, process, setProcess } = useMarvelService();
 
+  const listVariants = {
+    visible: (i) => ({
+      opacity: 1,
+      transition: {
+        delay: i * 0.1,
+      },
+    }),
+    hidden: {
+      opacity: 0,
+    },
+  };
 
-    const listVariants = {
-        visible: i => ({
-            opacity: 1,
-            transition: {
-                delay: i * 0.1,
+  useEffect(() => {
+    onRequest(offset, true);
+  }, []);
+
+  const onRequest = (offset, initial) => {
+    initial ? setNewItemLoading(false) : setNewItemLoading(true);
+    getAllCharacters(offset)
+      .then(onCharListLoaded)
+      .then(() => setProcess("confirmed")); //FSM
+  };
+
+  const onCharListLoaded = (newCharList) => {
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
+    setCharList((charList) => [...charList, ...newCharList]);
+    setNewItemLoading((newItemLoading) => false);
+    setOffset((offset) => offset + 9);
+    setCharEnded((charEnded) => ended);
+  };
+
+  const itemRefs = useRef([]);
+
+  const focusOnItem = (id) => {
+    itemRefs.current.forEach((item) =>
+      item.classList.remove("char__item_selected")
+    );
+    itemRefs.current[id].classList.add("char__item_selected");
+    itemRefs.current[id].focus();
+  };
+
+  function renderItems(arr) {
+    const items = arr.map((item, i) => {
+      let imgStyle = { objectFit: "cover" };
+      if (
+        item.thumbnail ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+      ) {
+        imgStyle = { objectFit: "unset" };
+      }
+      return (
+        <motion.li
+          className="char__item"
+          tabIndex={0}
+          ref={(el) => (itemRefs.current[i] = el)}
+          key={item.id}
+          variants={listVariants}
+          initial="hidden"
+          animate="visible"
+          custom={i}
+          onClick={() => {
+            props.onCharSelected(item.id);
+            focusOnItem(i);
+          }}
+          onKeyPress={(e) => {
+            if (e.key === " " || e.key === "Enter") {
+              props.onCharSelected(item.id);
+              focusOnItem(i);
             }
-        }),
-        hidden: {
-            opacity: 0
-        }
-    }
+          }}
+        >
+          <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+          <div className="char__name">{item.name}</div>
+        </motion.li>
+      );
+    });
 
+    return <ul className="char__grid">{items}</ul>;
+  }
 
-    useEffect(() => {
-        onRequest(offset, true);
-    }, [])
- 
-    const onRequest = (offset, initial) => {
-        initial ? setNewItemLoading(false) : setNewItemLoading(true);
-        getAllCharacters(offset)
-            .then(onCharListLoaded)
-            .then(() => setProcess('confirmed')); //FSM
+  const elements = useMemo(() => {
+    return setContent(process, () => renderItems(charList), newItemLoading);
+  }, [process]);
 
-            
-    }
-
-    const onCharListLoaded = (newCharList) => {
-
-        let ended = false;
-        if (newCharList.length < 9) {
-            ended = true;
-        }
-        setCharList(charList => [...charList, ...newCharList]);
-        setNewItemLoading(newItemLoading => false);
-        setOffset(offset => offset + 9);
-        setCharEnded(charEnded => ended)
-    }
-
-    const itemRefs = useRef([]);
-
-    const focusOnItem = (id) => {
-        itemRefs.current.forEach(item => item.classList.remove('char__item_selected'));
-        itemRefs.current[id].classList.add('char__item_selected');
-        itemRefs.current[id].focus();
-    }
-
-
-
-
-    function renderItems(arr) {
-        const items =  arr.map((item, i) => {
-            let imgStyle = {'objectFit' : 'cover'};
-            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-                imgStyle = {'objectFit' : 'unset'};
-            }
-            return (
-                <motion.li 
-                    className="char__item"
-                    tabIndex={0}
-                    ref={el => itemRefs.current[i] = el}
-                    key={item.id}
-                    variants={listVariants}
-                    initial='hidden'
-                    animate='visible'
-                    custom={i}
-                    onClick={() => {
-                        props.onCharSelected(item.id);
-                        focusOnItem(i);
-                    }}
-                    onKeyPress={(e) => {
-                        if (e.key === ' ' || e.key === "Enter") {
-                            props.onCharSelected(item.id);
-                            focusOnItem(i);
-                        }
-                    }}>
-                    <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-                    <div className="char__name">{item.name}</div>
-                </motion.li>
-            )
-        });
-
-        return (
-            <ul className="char__grid">
-                {items}
-            </ul>
-        )
-    }
-
-    const elements = useMemo(() => {
-        return setContent(process, () => renderItems(charList), newItemLoading)
-
-    }, [process]);
-     
-    return (
-        <div className="char__list">
-            {elements}
-            <button 
-                className="button button__main button__long"
-                disabled={newItemLoading}
-                style={{'display': charEnded ? 'none' : 'block'}}
-                onClick={() => onRequest(offset)}>
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
-}
+  return (
+    <div className="char__list">
+      {elements}
+      <button
+        className="button button__main button__long"
+        disabled={newItemLoading}
+        style={{ display: charEnded ? "none" : "block" }}
+        onClick={() => onRequest(offset)}
+      >
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
+};
 
 CharList.propTypes = {
-    onCharSelected: PropTypes.func.isRequired
-}
+  onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -1,116 +1,111 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import {motion} from "framer-motion/dist/framer-motion";
+import { motion } from "framer-motion";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import useMarvelService from "../../services/MarvelService";
 
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
-import useMarvelService from '../../services/MarvelService';
+import "./comicsList.scss";
 
-import './comicsList.scss';
-
-const setContent = (process, Component, newItemLoading) => { //FSM
-    switch (process) {
-        case 'waiting':
-            return <Spinner/>
-        case 'loading':
-            return newItemLoading ? <Component/> : <Spinner/>
-        case 'confirmed':
-            return <Component/>;
-        case 'error':
-            return <ErrorMessage/>;
-        default:
-            throw new Error('Unexpected process state')
-    }
-}  
+const setContent = (process, Component, newItemLoading) => {
+  //FSM
+  switch (process) {
+    case "waiting":
+      return <Spinner />;
+    case "loading":
+      return newItemLoading ? <Component /> : <Spinner />;
+    case "confirmed":
+      return <Component />;
+    case "error":
+      return <ErrorMessage />;
+    default:
+      throw new Error("Unexpected process state");
+  }
+};
 
 const ComicsList = () => {
+  const [comicsList, setComicsList] = useState([]);
+  const [newItemLoading, setNewItemLoading] = useState(false);
+  const [offset, setOffset] = useState(0);
+  const [comicsEnded, setComicsEnded] = useState(false);
 
-    const [comicsList, setComicsList] = useState([]);
-    const [newItemLoading, setNewItemLoading] = useState(false);
-    const [offset, setOffset] = useState(0);
-    const [comicsEnded, setComicsEnded] = useState(false);
+  const { getAllComics, process, setProcess } = useMarvelService();
 
-    const {getAllComics, process, setProcess} = useMarvelService();
+  const listVariants = {
+    visible: (i) => ({
+      opacity: 1,
+      transition: {
+        delay: i * 0.1,
+      },
+    }),
+    hidden: {
+      opacity: 0,
+    },
+  };
 
+  useEffect(() => {
+    onRequest(offset, true);
+  }, []);
 
-    const listVariants = {
-        visible: i => ({
-            opacity: 1,
-            transition: {
-                delay: i * 0.1,
-            }
-        }),
-        hidden: {
-            opacity: 0
-        }
+  const onRequest = (offset, initial) => {
+    initial ? setNewItemLoading(false) : setNewItemLoading(true);
+    getAllComics(offset)
+      .then(onComicsListLoaded)
+      .then(() => setProcess("confirmed")); //FSM
+  };
+
+  const onComicsListLoaded = (newComicsList) => {
+    let ended = false;
+    if (newComicsList.length < 8) {
+      ended = true;
     }
 
-    useEffect(() => {
-        onRequest(offset, true);
-    }, [])
+    setComicsList((comicList) => [...comicList, ...newComicsList]);
+    setNewItemLoading((newItemLoading) => false);
+    setOffset((offset) => offset + 8);
+    setComicsEnded((comicsEnded) => ended);
+  };
 
-    const onRequest = (offset, initial) => {
-        initial ? setNewItemLoading(false) : setNewItemLoading(true);
-        getAllComics(offset)
-            .then(onComicsListLoaded)
-            .then(() => setProcess('confirmed')); //FSM
+  function renderItems(arr) {
+    const items = arr.map((item, i) => {
+      return (
+        <motion.li
+          className="comics__item"
+          key={i}
+          variants={listVariants}
+          initial="hidden"
+          animate="visible"
+          custom={i}
+        >
+          <Link to={`/comics/${item.id}`}>
+            <img
+              src={item.thumbnail}
+              alt={item.title}
+              className="comics__item-img"
+            />
+            <div className="comics__item-name">{item.title}</div>
+            <div className="comics__item-price">{item.price}</div>
+          </Link>
+        </motion.li>
+      );
+    });
+    return <ul className="comics__grid">{items}</ul>;
+  }
 
-        
-    }
-    
-    const onComicsListLoaded = (newComicsList) => {
-        let ended = false;
-        if (newComicsList.length < 8) {
-            ended = true;
-        }
-
-        setComicsList(comicList => [...comicList, ...newComicsList]);
-        setNewItemLoading(newItemLoading => false);
-        setOffset(offset => offset + 8);
-        setComicsEnded(comicsEnded => ended)
-    }
-
-    function renderItems(arr) {
-        const items = arr.map((item, i) => {
-            return (
-                <motion.li 
-                    className="comics__item" 
-                    key={i}
-                    variants={listVariants}
-                    initial='hidden'
-                    animate='visible'
-                    custom={i}>
-                    <Link to={`/comics/${item.id}`}>
-                        <img src={item.thumbnail} alt={item.title} className="comics__item-img"/>
-                        <div className="comics__item-name">{item.title}</div>
-                        <div className="comics__item-price">{item.price}</div>
-                    </Link>
-                </motion.li>
-            )
-        })
-        return (
-            <ul className="comics__grid">
-              {items}  
-            </ul>
-        )
-
-
-    }
-
-
-    return (
-        <div className="comics__list">
-            {setContent(process, () => renderItems(comicsList), newItemLoading)}
-            <button 
-            className="button button__main button__long"
-            disabled={newItemLoading}
-            style={{'display': comicsEnded ? 'none' : 'block'}}
-            onClick={() => onRequest(offset)}>
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
-}
+  return (
+    <div className="comics__list">
+      {setContent(process, () => renderItems(comicsList), newItemLoading)}
+      <button
+        className="button button__main button__long"
+        disabled={newItemLoading}
+        style={{ display: comicsEnded ? "none" : "block" }}
+        onClick={() => onRequest(offset)}
+      >
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
+};
 
 export default ComicsList;
